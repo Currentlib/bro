1569b5652dfe1dc8045627e7b48803714f7205d3
Currentlib <11hacha11@gmail.com>
2016-10-25 02:48:26 +0300

    RPS

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    cout << "Hello! Lets play RPS." << endl;
+    string again = "yes";
+    while (again == "yes")
+    {
+    int user;
+    int pc;
+    cout << "Select Rock - 1; Papper - 2; Scissors - 3: ";
+    cin >> user;
+    pc = 1+(rand()%3);
+    cout << pc << endl;
+    if ((user == 1 && pc == 2) || (user == 2 && pc == 3) || (user == 3 && pc == 1))
+    {
+        cout << "You're LOOOOOOOOOSEEEEEEER!!!!!!!!!111111111111OONEONEONEOneoneone" << endl;
+    } else if ((user == 1 && pc == 1) || (user == 2 && pc == 3) || (user == 3 && pc == 3))
+    {
+        cout << "Round draw." << endl;
+    } else
+    {
+        cout << "We have winner.";
+    }
+    cout << "Again? (yes/no): ";
+    cin >> again;
+    }
 }
